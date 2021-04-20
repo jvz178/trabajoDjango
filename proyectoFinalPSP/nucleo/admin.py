cdb42ddaa982961e647a8489ea6f9494d79fd05a
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-04-21 00:48:23 +0200

    primerCommit

---
+++ proyectoFinalPSP/nucleo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from nucleo.models import Usuario, Cliente, Especialista, Cita, Mensaje
+
+admin.site.register(Usuario)
+admin.site.register(Cliente)
+admin.site.register(Especialista)
+admin.site.register(Cita)
+admin.site.register(Mensaje)
---
+++ proyectoFinalPSP/nucleo/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+class Usuario(models.Model):
+    username=models.CharField(max_length=30)
+    password=models.IntegerField()
+
+class Cliente(models.Model):
+    dni=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=100)
+    fechaNacimiento=models.DateField()
+    foto=models.CharField(max_length=100)
+    idUsuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+
+class Especialista(models.Model):
+    dni=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=50)
+    apellidos=models.CharField(max_length=100)
+    direccion=models.CharField(max_length=100)
+    fechaNacimiento=models.DateField()
+    foto=models.CharField(max_length=100)
+    biografia=models.CharField(max_length=255)
+    idUsuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+
+class Cita(models.Model):
+    fecha=models.DateField()
+    idCliente=models.ForeignKey(Cliente,on_delete=models.CASCADE)
+    idEspecialista=models.ForeignKey(Especialista,on_delete=models.CASCADE)
+    informe=models.TextField()
+    realizada=models.BooleanField()
+
+class Mensaje(models.Model):
+    idEmisor=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    idReceptor=models.ForeignKey(Usuario,related_name='%(class)s_requests_created',on_delete=models.CASCADE)
+    fecha=models.DateField()
+    asunto=models.CharField(max_length=50)
+    texto=models.TextField()
+    leido=models.BooleanField()
