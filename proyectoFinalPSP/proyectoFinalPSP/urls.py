f588cb47f234439068ae8b617dd3ab1e0df3ff6e
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-04-21 23:31:22 +0200

    commit5

--- proyectoFinalPSP/proyectoFinalPSP/urls.py
+++ proyectoFinalPSP/proyectoFinalPSP/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('jet/',include('jet.urls','jet')),
     path('admin/', admin.site.urls),
-]
+    path('',include('nucleo.urls')),
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
