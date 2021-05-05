37227cedf1cd6ae9f0ed0584c2b0c3f9349f422f
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-05-05 23:17:08 +0200

    c

--- proyectoFinalPSP/nucleo/urls.py
+++ proyectoFinalPSP/nucleo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.inicio,name="inicio"),
     path('logueado',views.logueado,name="logueado"),
+    path('editarUsuario/<int:pk>',views.updateUser.as_view(), name="editarUsuario"),
 ]
--- proyectoFinalPSP/nucleo/views.py
+++ proyectoFinalPSP/nucleo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.models import User
 
 def inicio(request):
     return render(request,'nucleo/inicio.html')
 
 def logueado(request):
     return render(request,'nucleo/logueado.html')
+
+class updateUser(UpdateView):
+    model=User
+    fields=['username','password']
+    success_url="/logueado"
