d55f9c76f69bb2ad8f930c8603af89f0d16a329f
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-05-05 12:16:45 +0200

    c

--- proyectoFinalPSP/registration/views.py
+++ proyectoFinalPSP/registration/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 class SignupView(CreateView):
     form_class=UserCreationForm
@@ -15,7 +16,7 @@ class SignupView(CreateView):
                                          'placeholder':'Introduce el nombre de usuario'})
         form.fields['password1'].widget=forms.PasswordInput(attrs={'class':'form-control mb2',
                                                     'placeholder':'Introduce una contraseña'})
-        form.fields['password2'].widget=forms.TextInput(attrs={'class':'form-control mb2',
+        form.fields['password2'].widget=forms.PasswordInput(attrs={'class':'form-control mb2',
                                         'placeholder':'Introduce la contraseña de nuevo'})
 
         return form
