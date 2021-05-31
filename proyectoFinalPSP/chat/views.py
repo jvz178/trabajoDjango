208ca23899664b5da3232df7c43c4161d6208497
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-05-31 19:34:38 +0200

    commit

--- proyectoFinalPSP/chat/views.py
+++ proyectoFinalPSP/chat/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+crearEscribirFichero=""
+
 def chat(request,especialistaId,usuarioId):
-
+    global crearEscribirFichero
     direccion='chat/ficheros/'+str(usuarioId)+'-'+str(especialistaId)+'.txt'
-    crearFichero = open(direccion,'a')
+    crearEscribirFichero = open(direccion,'a')
     leerFichero = open(direccion,'r')
-    fichero=leerFichero.read()
+    fichero=leerFichero.read().split("<sig>")
+    return render(request,"chat.html",{'fichero':fichero})
 
+def mensaje(request):
+    global crearEscribirFichero
+    print(request)
     return render(request,"chat.html")
