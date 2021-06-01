ef110d2c9d668e53bb8a366d2b94da88ca13250e
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-06-01 14:24:11 +0200

    commit

--- proyectoFinalPSP/chat/views.py
+++ proyectoFinalPSP/chat/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
 
-crearEscribirFichero=""
 direccion=""
 
 def chat(request,especialistaId,usuarioId):
-    global crearEscribirFichero, direccion
+    global direccion
     direccion='chat/ficheros/'+str(usuarioId)+'-'+str(especialistaId)+'.txt'
     crearEscribirFichero = open(direccion,'a')
     fichero=mostrarMensajes()
     return render(request,"chat.html",{'fichero':fichero})
 
 def guardarMensaje(request, user):
-    global crearEscribirFichero, direccion
-    mensaje="\n"+str(user)+": "+request.POST.get('comentario','')+" (/)<sig>"
-    crearEscribirFichero = open(direccion,'a')
-    crearEscribirFichero.write(mensaje)
+    global direccion
+    mensaje=str(user)+": "+request.POST.get('comentario','')+" (/)"
+    abrirFichero = open(direccion,'a')
+    abrirFichero.write("\n"+mensaje+"<sig>")
     fichero=mostrarMensajes()
+    fichero.append(mensaje)
     return render(request,"chat.html",{'fichero':fichero})
 
 def mostrarMensajes():
+    crearEscribirFichero = open(direccion,'a')
     leerFichero = open(direccion,'r')
     fichero=leerFichero.read().split("<sig>")
     return fichero
